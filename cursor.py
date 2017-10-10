c2f819981ab9eb6380cef73908d76eed78abcd3e
michael <michael@thegur.net>
2017-10-10 10:35:56 +0300

    ignore

--- cursor.py
+++ cursor.py
@@ -3,18 +3,21 @@ import math
 
 
 class Cursor(object):
-    indices = [0, 1, 2, 3, 4]
+    instances = []
+    selected = None
+    _on_select = None
 
     def __init__(self, canvas, cmd=None, x=100, y=100):
-        if not Cursor.indices:
+        if not Cursor.can_create():
             print 'cannot create more than 5 lasers'
             return
 
         self.x, self.y = x, y
         self.r, self.hr = 15, 4
         self.hx, self.hy = x - self.r + self.hr / 2, y - self.r + self.hr / 2
-        self.index = Cursor.indices.pop()
-        self.id, self.hid = 'ring%i' % self.index, 'handle%i' % self.index
+        index = len(Cursor.instances)
+        self.id, self.hid, self.rid = 'ring%i' % index, 'handle%i' % index, "resize%i" % index
+        Cursor.instances.append(self)
 
         self.canvas = canvas
         assert isinstance(self.canvas, tk.Canvas)
@@ -23,14 +26,29 @@ class Cursor(object):
                                             width=3, outline='#000000')
         self.handle = self.canvas.create_oval(self.hx-self.hr, self.hy-self.hr, self.hx+self.hr, self.hy+self.hr,
                                               fill='#FFFFFF', tags=self.hid)
+        self.resizer = self.canvas.create_oval(self.x+self.r+2, self.y+self.r+2, self.x+self.r-2, self.y+self.r-2,
+                                              fill='#FFFFFF', tags=self.rid)
 
+        self.canvas.tag_bind(self.id, '<Button-1>', self.select)
         self.canvas.tag_bind(self.id, '<B1-Motion>', self.render)
         self.canvas.tag_bind(self.hid, '<B1-Motion>', self.rotate)
+        self.canvas.tag_bind(self.rid, '<B1-Motion>', self.resize)
 
         def c(_):
             if cmd is not None: cmd(self)
             self.destroy()
         self.canvas.tag_bind(self.id, '<Double-Button-1>', c)
+        self.select()
+
+    def resize(self, e):
+
+
+    def select(self, _=None):
+        for c in Cursor.instances:
+            self.canvas.itemconfig(c.center, outline='#000000')
+        self.canvas.itemconfig(self.center, outline='#FFFFFF')
+        Cursor.selected = self
+        if Cursor._on_select is not None: Cursor._on_select(self)
 
     def rotate(self, e):
         dx, dy = e.x - self.x, e.y - self.y
@@ -54,7 +72,13 @@ class Cursor(object):
         self.canvas.delete(self.ring)
         self.canvas.delete(self.center)
         self.canvas.delete(self.handle)
-        Cursor.indices.append(self.index)
+        Cursor.instances.remove(self)
+        if Cursor.selected is self:
+            Cursor.selected = None
+            if Cursor._on_select is not None: Cursor._on_select(None)
 
     @staticmethod
-    def can_create(): return len(Cursor.indices) != 0
+    def can_create(): return len(Cursor.instances) < 5
+
+    @staticmethod
+    def set_on_select(f): Cursor._on_select = staticmethod(f)
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ x, y = 0, 0
 
 
 def poo():
-    global c, img, sprite, x, y
+    global c, img, sprite, x, y, lazerbtn
     fn = askopenfilename()
     if fn == '': return
     img = Image.open(fn)
@@ -23,16 +23,18 @@ def poo():
     c.handle = ImageTk.PhotoImage(img)
     sprite = c.create_image(w / 2, h / 2, image=c.handle)
     Cursor.indices = [0, 1, 2, 3, 4]
+    lazerbtn.config(state=tk.ACTIVE)
 
 
 def lens(cursor):
-    global c, img, sprite, lazerbtn
+    global c, img, sprite, lazerbtn, createbtn
     c.delete(sprite)
     lens_flare(img, cursor.x, cursor.y, cursor.get_angle())
     c.handle = ImageTk.PhotoImage(img)
     sprite = c.create_image(img.size[0] / 2, img.size[1] / 2, image=c.handle)
     c.lower(sprite)
     lazerbtn.config(state=tk.ACTIVE)
+    createbtn.config(state=tk.DISABLED)
 
 
 def create_cursor():
@@ -40,8 +42,21 @@ def create_cursor():
     Cursor(c, lens)
     if not Cursor.can_create(): lazerbtn.config(state=tk.DISABLED)
 
+
+def on_select(cursor):
+    global createbtn
+    if cursor is None: return
+
+    def gen():
+        lens(Cursor.selected)
+        Cursor.selected.destroy()
+    createbtn.config(state=tk.ACTIVE, command=gen)
+Cursor.set_on_select(on_select)
+
 tk.Button(root, text='open', command=poo).pack()
-lazerbtn = tk.Button(root, text='lazer', command=create_cursor)
+lazerbtn = tk.Button(root, text='add', command=create_cursor, state=tk.DISABLED)
 lazerbtn.pack()
+createbtn = tk.Button(root, text='generate', state=tk.DISABLED)
+createbtn.pack()
 
 root.mainloop()
